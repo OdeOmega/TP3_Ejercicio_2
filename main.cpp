20d70548bc975a2b08b3cf5dd7c9bf8079d9e878
OdeOmega <agustinhermosillabaez@gmail.com>
2023-10-29 17:47:31 -0300

    Vamos de cero

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
+int INF = numeric_limits<int>::max();
+
+void eliminarAristas(vector<vector<int>> grafo){
+
+}
 
 int main() {
     int cantTest, cantPC, latencia;
     cin >> cantTest;
     for(int i = 0; i < cantTest; i++){
         cin >> cantPC;
-        //tipo de estructura
+        vector<vector<int>> grafo(cantPC,vector<int>(cantPC,INF));
         for(int j = 0; j < cantPC; j++){
             for(int k = 0; k < j; k++){
-                //aca la voy almacenando
+                cin >> latencia;
+                grafo[j][k] = latencia;
+                grafo[k][j] = latencia;
                 }
             }
         }
